bd1578aa3fd676d64194702620b3736ccaed4daa
dang2947 <gauthierdani@my.easternct.edu>
2017-10-23 17:20:14 -0400

    daniel_gauthier changes
    hello world

--- hello.java
+++ hello.java
@@ -1,18 +1,19 @@
 // a simple program to practice common Github commands 
-
+import java.util.Scanner;
 public class hello {
 
     public static void main(String [] args) {
-
-
 	
-	// added  line	
-	// added another line
-
-        System.out.println("Hello Class!");
-
+	Scanner reader = new Scanner(System.in);
+	System.out.prinln("Enter your name: ");
+	first_name = reader.next();
+	
+	reader.close();
+	System.out.println("Hello," + first_name + "my name is Dan")
 
 
     }
 }
+
+
 
